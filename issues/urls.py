752983b7608343c7791f5f239e489fdbed3cb1a1
malihe hajihosseini <m.hajihosseini95@gmail.com>
2015-05-19 04:14:47 -0700

    Update urls.py

--- issues/urls.py
+++ issues/urls.py
@@ -3,8 +3,8 @@ from django.contrib.auth.decorators import login_required as LR
 from .models import *
 from .views import *
 urlpatterns = patterns('',
+    url(r"^newProject/(?P<pk>\d+)/$", LR(NewProject.as_view()), name="newProject"),
     url(r"^profile/(?P<pk>\d+)/$", LR(EditProfile.as_view()), name="profile"),
-    url(r"^viewprofile/(?P<pk>\d+)/$" , LR(UserPageView.as_view()), name="viewprofile"),
     url(r"^forum/(?P<dpk>\d+)/$", LR(ForumView.as_view()), name= "forum"),
     url(r"^topic/(?P<dpk>\d+)/$", LR(TopicView.as_view()), name= "topic"),
     url(r"^forums$", LR(ForumList.as_view()), name="forums"),
