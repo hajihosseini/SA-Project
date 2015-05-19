0aae33ecf29667fd2a2b0641244bac0b6105e5f1
malihe hajihosseini <m.hajihosseini95@gmail.com>
2015-05-19 04:16:53 -0700

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^issues/', include('issues.urls', namespace="issues")),#issues urls
-    url(r'^admin/', include(admin.site.urls)),#admin urls
-    (r'^accounts/', include('allauth.urls')), #allauth urls
+    url(r'^issues/', include('issues.urls', namespace="issues")),
+    url(r'^admin/', include(admin.site.urls)),
+    (r'^accounts/', include('allauth.urls')),
 )
