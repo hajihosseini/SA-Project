97c60eda37d393f4103d5046ae34c1d1f92761c3
negar mohamadhassan <nemote95@users.noreply.github.com>
2015-05-20 16:54:35 +0430

    Delete forms.py

--- issues/forms.py
+++
@@ -1,6 +0,0 @@
-from django.forms import ModelForm
-from issues.models import *
-class ProjectForm(ModelForm):
-    class Meta:
-        model = Project
-        fields=['ProjectName','projectDescription','projectAccessType']
