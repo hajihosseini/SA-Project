1cf9e9885d260f2423561e0d4b6cd5be8393d8fa
negar mohamadhassan <nemote95@users.noreply.github.com>
2015-05-20 16:53:30 +0430

    Update urls.py

--- issues/urls.py
+++ issues/urls.py
@@ -3,10 +3,15 @@ from django.contrib.auth.decorators import login_required as LR
 from .models import *
 from .views import *
 urlpatterns = patterns('',
+    url(r"^taskPage/(?P<pk>\d+)/$", LR(TaskDone.as_view()), name="taskPage"),
+    url(r"^newTopic/(?P<pk>\d+)/$", LR(NewTopic.as_view()), name="newTopic"),
+    url(r"^projectPage/(?P<pk>\d+)/$", LR(ProjectView.as_view()), name="projectPage"),
+    url(r"^newTask/(?P<pk>\d+)/$", LR(NewTask.as_view()), name="newTask"),
     url(r"^newProject/(?P<pk>\d+)/$", LR(NewProject.as_view()), name="newProject"),
     url(r"^profile/(?P<pk>\d+)/$", LR(EditProfile.as_view()), name="profile"),
-    url(r"^forum/(?P<dpk>\d+)/$", LR(ForumView.as_view()), name= "forum"),
-    url(r"^topic/(?P<dpk>\d+)/$", LR(TopicView.as_view()), name= "topic"),
+    url(r"^forum/(?P<pk>\d+)/$", LR(ForumView.as_view()), name="forum"),
+    url(r"^topic/(?P<pk>\d+)/$", LR(TopicView.as_view()), name="topic"),
+    url(r"^newPost/(?P<pk>\d+)/$", LR(NewPost.as_view()), name="newPost"),
     url(r"^forums$", LR(ForumList.as_view()), name="forums"),
-    url(r"^$", Main.as_view(), name="main"),
+    url(r"^$", Home.as_view(), name="home"),
 )
