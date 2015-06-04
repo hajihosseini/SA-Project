23751ba4769fb2ee2e2e77c5a0356b0b364636d0
negar mohamadhassan <nemote95@users.noreply.github.com>
2015-06-04 21:00:53 +0430

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -127,6 +127,6 @@ STATIC_URL = '/static/'
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "pyissues.noreply@gmail.com"
-EMAIL_HOST_PASSWORD = "pyissues123"
+EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
