d6b0953f5346d573b3debbd5068ef6d4608ee6e6
Zahra Momeni <hzhmomeni@gmail.com>
2015-06-06 09:12:01 +0430

    Create z.js

---
+++ issues/static/js/z.js
@@ -0,0 +1,15 @@
+
+/* js for home */
+$(document).ready(function() {
+    $('.collapse').on('show.bs.collapse', function() {
+        var id = $(this).attr('id');
+        $('a[href="#' + id + '"]').closest('.panel-heading').addClass('active-faq');
+        $('a[href="#' + id + '"] .panel-title span').html('<i class="glyphicon glyphicon-minus"></i>');
+    });
+    $('.collapse').on('hide.bs.collapse', function() {
+        var id = $(this).attr('id');
+        $('a[href="#' + id + '"]').closest('.panel-heading').removeClass('active-faq');
+        $('a[href="#' + id + '"] .panel-title span').html('<i class="glyphicon glyphicon-plus"></i>');
+    });
+});
+/* end */
